8be9b115180285a0d454dd3b9e910efbf0ee82e6
Patricia Padilha <patriciapadilhabio@gmail.com>
2022-10-20 17:10:34 -0300

    feat(insights): implement the function matches_salary_range

--- src/insights.py
+++ src/insights.py
@@ -169,7 +169,16 @@ def matches_salary_range(job, salary):
         If `job["min_salary"]` is greather than `job["max_salary"]`
         If `salary` isn't a valid integer
     """
-    pass
+    if "min_salary" not in job or "max_salary" not in job:
+        raise ValueError("min_salary or max_salary doesn't exists")
+    if type(job["min_salary"]) is not int or type(job["max_salary"]) != int:
+        raise ValueError("min_salary or max_salary aren't valid integers")
+    if job["min_salary"] > job["max_salary"]:
+        raise ValueError("min_salary is greather than max_salary")
+    if type(salary) is not int:
+        raise ValueError("salary isn't a valid integer")
+
+    return (job["min_salary"] <= salary <= job["max_salary"])
 
 
 def filter_by_salary_range(jobs, salary):
